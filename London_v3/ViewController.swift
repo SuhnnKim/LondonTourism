519a04e73b4ca985674e2c1bacc39c844ba333ef
KimSuhn Mi <kim.suhnmi@gmail.com>
2016-03-15 23:18:57 -0400

    Initial Commit

---
+++ London_v3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  London_v3
+//
+//  Created by KimSuhn Mi on 2016-03-15.
+//  Copyright © 2016 KimSuhn Mi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
