d838359faee1158bc81334dc69c76d91d34e41bd
KimSuhn Mi <kim.suhnmi@gmail.com>
2016-03-20 17:17:43 -0400

    at least STAY screen doesn't crash

--- London_v3/MainTableViewController.swift
+++ London_v3/MainTableViewController.swift
@@ -16,16 +16,8 @@ class MainTableViewController: UITableViewController {
     //weak var shopCellCtl : ShopTableViewCell!
     var shopCellCtl:ShopTableViewCell = ShopTableViewCell()
     
-
-    
     override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        super.viewDidLoad()    
         
         // nav image
         let navImage:UIImage = UIImage(named: "banner.jpg")!
--- London_v3/StayViewController.swift
+++ London_v3/StayViewController.swift
@@ -8,24 +8,57 @@
 
 import UIKit
 
-class StayViewController: UIViewController {
+class StayViewController: UIViewController, UITableViewDataSource{
 
     @IBOutlet weak var navView: UIView!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var list = [Category]()
+    var stayCellCtl:ShopTableViewCell = ShopTableViewCell()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // nav logo and image
         let navImage:UIImage = UIImage(named: "banner.jpg")!
         let navImageView:UIImageView = UIImageView(image: navImage)
         navView.addSubview(navImageView)
-
+        
+        // cell items
+        list = stayCellCtl.loadList("Stay")
+        
+        // populate the table
+        tableView.registerClass(ShopTableViewCell.self, forCellReuseIdentifier: "StayTableViewCell")
+        tableView.dataSource = self
     }
+    
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        return list.count
+    }
+    
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell {
+        let cellIdentifier = "ShopTableViewCell"
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! StayTableViewCell
+        
+        // Fetches the appropriate meal for the data source layout
+        let store = list[indexPath.row]
+        
+        cell.nameLabel.text = store.name
+        cell.shopImageView.image = store.photo
+        cell.addressLabel.text = store.address
+        cell.phoneLabel.text = store.phone
+        return cell
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    
+    
+
 
     /*
     // MARK: - Navigation
