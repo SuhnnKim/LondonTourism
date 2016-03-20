2c99a877b920b5911108bc81deab538db8a4b49b
KimSuhn Mi <kim.suhnmi@gmail.com>
2016-03-20 16:16:14 -0400

    Testing combining View + Table View in a View controller (Stay screen)

---
+++ London_v3/StayViewController.swift
@@ -0,0 +1,40 @@
+//
+//  StayViewController.swift
+//  London_v3
+//
+//  Created by KimSuhn Mi on 2016-03-20.
+//  Copyright © 2016 KimSuhn Mi. All rights reserved.
+//
+
+import UIKit
+
+class StayViewController: UIViewController {
+
+    @IBOutlet weak var navView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // nav logo and image
+        let navImage:UIImage = UIImage(named: "banner.jpg")!
+        let navImageView:UIImageView = UIImageView(image: navImage)
+        navView.addSubview(navImageView)
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
