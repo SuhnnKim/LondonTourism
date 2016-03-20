8fada12fc8df1622608344adcc37a32fb4c5ecc5
KimSuhn Mi <kim.suhnmi@gmail.com>
2016-03-20 19:16:11 -0400

    STAY screen works

--- London_v3/StayViewController.swift
+++ London_v3/StayViewController.swift
@@ -8,57 +8,62 @@
 
 import UIKit
 
-class StayViewController: UIViewController, UITableViewDataSource{
+class StayViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var navView: UIView!
     @IBOutlet weak var tableView: UITableView!
-    
-    var list = [Category]()
-    var stayCellCtl:ShopTableViewCell = ShopTableViewCell()
+    //var list = [Category]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // nav logo and image
+
+        // nav image
         let navImage:UIImage = UIImage(named: "banner.jpg")!
         let navImageView:UIImageView = UIImageView(image: navImage)
         navView.addSubview(navImageView)
         
-        // cell items
-        list = stayCellCtl.loadList("Stay")
-        
-        // populate the table
-        tableView.registerClass(ShopTableViewCell.self, forCellReuseIdentifier: "StayTableViewCell")
-        tableView.dataSource = self
+        // datasource, delegate
+//        tableView.dataSource = self
+//        tableView.delegate = self
     }
-    
-    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
-        return list.count
-    }
-    
-    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell {
-        let cellIdentifier = "ShopTableViewCell"
-        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! StayTableViewCell
-        
-        // Fetches the appropriate meal for the data source layout
-        let store = list[indexPath.row]
-        
-        cell.nameLabel.text = store.name
-        cell.shopImageView.image = store.photo
-        cell.addressLabel.text = store.address
-        cell.phoneLabel.text = store.phone
-        return cell
-    }
-
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    // MARK: - Table view data source
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 1
+    }
     
     
-
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cellIdentifier = "ShopTableViewCell"
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! ShopTableViewCell
+        
+        // Fetches the appropriate meal for the data source layout
+        //let store = list[indexPath.row]
+        
+        cell.nameLabel.text = "Paul's inn"
+        cell.shopImageView.image = UIImage(named: "molly")
+        cell.addressLabel.text = "Joe shuster"
+        cell.phoneLabel.text = "1234"
+        //cell.typeLabel.text = (Shop)store.type?
+        
+        
+        return cell
+    }
+    
+    
 
     /*
     // MARK: - Navigation
