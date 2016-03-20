3705e3a5292a24363753c81b9732c67b3e9fc238
KimSuhn Mi <kim.suhnmi@gmail.com>
2016-03-20 15:32:48 -0400

    Buttons coloured

--- London_v3/NavigationController.swift
+++ London_v3/NavigationController.swift
@@ -1,5 +1,5 @@
 //
-//  TabBarController.swift
+//  NavigationController.swift
 //  London_v3
 //
 //  Created by KimSuhn Mi on 2016-03-20.
@@ -8,17 +8,12 @@
 
 import UIKit
 
-class TabBarController: UITabBarController {
+class NavigationController: UINavigationController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //self.tabBar.frame = CGRect(x: 0, y: self.view.frame.size.height-95, width:  self.view.frame.size.width, height: 95)
-        UITabBar.appearance().barTintColor = UIColor(red: 0.102, green: 0.102, blue: 0.102, alpha: 1)
-        UITabBar.appearance().tintColor = UIColor(red: 0.957, green: 0.957, blue: 0.957, alpha: 1)
-        
 
-
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
