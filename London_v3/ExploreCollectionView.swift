e365f043cc4a0dc0f6eb1e78e1c3e32a7fbf0720
KimSuhn Mi <kim.suhnmi@gmail.com>
2016-03-24 12:22:08 -0400

    Collection view, collection cell added, hooked with classes and a view for detail

---
+++ London_v3/ExploreCollectionView.swift
@@ -0,0 +1,21 @@
+//
+//  ExploreCollectionView.swift
+//  London_v3
+//
+//  Created by KimSuhn Mi on 2016-03-24.
+//  Copyright © 2016 KimSuhn Mi. All rights reserved.
+//
+
+import UIKit
+
+class ExploreCollectionView: UICollectionView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ London_v3/ExploreCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ExploreCollectionViewCell.swift
+//  London_v3
+//
+//  Created by KimSuhn Mi on 2016-03-24.
+//  Copyright © 2016 KimSuhn Mi. All rights reserved.
+//
+
+import UIKit
+
+class ExploreCollectionViewCell: UICollectionViewCell {
+    
+}
